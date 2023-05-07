e7c7f421d9e36004be41070242e0659353ccd010
Theo Browne <me@t3.gg>
2023-05-06 18:30:08 -0700

    document code a bit more

--- src/app/2014.jsx
+++ src/app/2014.jsx
@@ -3,6 +3,9 @@
 import createReactClass from "create-react-class";
 import { doWork } from "@/app/action";
 
+// This is a component I found in an old React blog post from 2014
+// The only changes I had to make were to the ref binding syntax and
+// switching from React.createClass to createReactClass
 export var CommentForm = createReactClass({
   handleSubmit: function (e) {
     var author = this.authorRef.value.trim();
@@ -13,11 +16,12 @@ export var CommentForm = createReactClass({
     }
 
     console.log(author, text);
-    // TODO: send request to the server
     this.authorRef.value = "";
     this.textRef.value = "";
-    e.preventDefault();
+
+    // This is a server action from Next 13.4
     doWork({ name: author, contents: text });
+
     return false;
   },
   render: function () {
